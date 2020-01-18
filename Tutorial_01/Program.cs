9d66c355c3a260e4ff42cdc8bb504eb70257d54d
Caaakey <kbop2000@gmail.com>
2020-01-18 17:02:36 +0900

    Add Tutorial

--- Tutorial_01/Program.cs
+++ Tutorial_01/Program.cs
@@ -1,15 +1,146 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tutorial_01
 {
     class Program
     {
+        //  열거형 (Enum)
+        enum Weapon
+        {
+            None = 0,
+            LongSword,
+            Staff,
+            Bow,
+            Etc
+        }
+
         static void Main(string[] args)
         {
+            #region 조건문 및 반복문
+            //  초록색 글씨는 '주석'
+            //  Ctrl + Shift + B 전체 빌드 단축키
+            //  Ctrl + F5        빌드 후 실행
+
+            //  자료형 ?
+            //  int     -21억 부터 21억 까지 들어감
+            //  float   부동 소수점. 소수점 7자리(6자리)
+            //  double  부동 소수점. 소수점 16자리(12자리)
+            //  bool    ture 또는 false 값만 들어가는 자료형
+            //  string  문자열을 넣을 수 있는 자료형
+            //  char    -126 ~ 126
+            //  unsinged char 0 ~ 256
+
+            //int value = 0;
+            //value = value + 1;
+            //value += 1;
+
+            //value = value - 1;
+            //value -= 1;
+
+            //value = value * 1;
+            //value *= 1;
+
+            //value = value / 1;
+            //value /= 1;
+
+            //  조건문 ?
+            //  value == 1 이면 Ok 를 출력하고
+            //  아니면 Flase 를 출력해보세요!
+            //  출력하는 방법?
+            //if (value == 1) Console.WriteLine("Ok");
+            //else Console.WriteLine("False");
+
+            ////  반복문 ?
+            ////  for, while
+            //for (int i = 0; i <= 10; ++i)
+            //{
+            //    Console.WriteLine(i.ToString());
+            //    //  printf("%d\n", i);
+            //}
+
+            //  전체 주석하기
+            //  Ctrl + K + C
+            //  주석 풀기
+            //  Ctlr + K + U
+
+            //  i 는 0 부터 5 까지 순회하되
+            //  i 가 2면 OK 를 출력하고
+            //  아니면 False 를 출력해보세요
+
+            //for (int x = 0; x < 5; x++)
+            //{
+            //    if (x == 2) Console.WriteLine("Ok");
+            //    else Console.WriteLine("False");
+            //}
+
+            //  Gugudan!
+            //  1단부터 9단까지 출력해보세요
+            //  1 x 1 = 1
+            //  1 x 2 = 2
+            //  ...
+            //  9 x 9 = 81
+
+            //for (int i = 1; i < 10; ++i)
+            //{
+            //    for (int j = 1; j < 10; ++j)
+            //    {
+            //        Console.WriteLine("{0} x {1} = {2}", i, j, i * j);
+            //    }
+            //}
+            #endregion
+
+            //  switch ?
+            //  조건문 + 열거형 (Enum)
+
+
+            //  내가 지금 장착하고있는 무기
+            //Weapon weapon = Weapon.None;
+            ////weapon = default(Weapon);
+
+            //string str = string.Empty;
+            //str = Console.ReadLine();
+
+            //Console.WriteLine("내가 장착한 무기 : " + weapon.ToString());
+
+            //Weapon findWeapon = Weapon.None;
+            //Enum.TryParse(str, out findWeapon);
+
+            ////  case, break, default
+            //switch (findWeapon)
+            //{
+            //    case Weapon.LongSword:
+            //        weapon = Weapon.LongSword;
+            //        break;
+
+            //    case Weapon.Staff:
+            //        weapon = Weapon.Staff;
+            //        break;
+
+            //    case Weapon.Bow:
+            //        weapon = Weapon.Bow;
+            //        break;
+
+            //    default:
+            //        weapon = Weapon.Etc;
+            //        break;
+            //}
+
+            //Console.WriteLine("새로 장착한 무기 : " + weapon.ToString());
+
+            //  while ?
+            //  반복문
+
+            //int value = 0;
+            //while (value != 100)
+            //{
+            //    value++;
+            //    Console.WriteLine(value.ToString());
+            //}
+
+            //  솔루션 탐색기 단축키
+            //  Ctrl + Alt + L
         }
     }
+
 }
---
+++ VendingMachine/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        //  열거형 자리
+
+        static void Main(string[] args)
+        {
+            //  자판기 프로젝트!
+            int money = 10000;
+
+            string input = "Start";
+            while (!string.IsNullOrEmpty(input))
+            {
+                Console.Clear();
+                Console.WriteLine("소지금 : {0}", money);
+                Console.WriteLine("어서오세요 자판기입니다");
+                //  1. 상품 보여주기!
+
+                //  2. 입력 받기!
+                input = Console.ReadLine();
+
+                //  맨 위에 using System.Threading; 추가하기
+                Thread.Sleep(1000); //  1초동안 기다린다
+
+                //  열거행을 사용하여 자판기를 만들어보세요!
+                //  이름 : Cola, Sida, Fanta, Water, Coffee, Tea
+                //  가격 : 100,  200,  150,   80,     2000,   50
+
+                //string[] str = Enum.GetNames(typeof(Drink));
+
+                //  3. 계산하기!
+                //  대출 불가! 마이너스 통장 불가!
+                //  무조건 정가
+            }
+
+            Console.WriteLine("자판기 종료!");
+        }
+    }
+}
